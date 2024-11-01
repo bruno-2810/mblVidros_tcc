ab02bd79be7daa8dc5d6b3b149171c5da882072e
bruno <ra55498652895@acaonsfatima.org.br>
2024-11-01 07:34:11 -0300

    freiiii

--- src/components/secao3/index.jsx
+++ src/components/secao3/index.jsx
@@ -1,23 +1,23 @@
 import './index.scss'
 
-export default function Secao3(){
+export default function Secao3() {
 
-    return(
+    return (
         <div className="secao3">
 
             <div className='porque'>
-                <h1>Por que Escolher a Vidraçaria <span>MBL vidros?</span> </h1>
+                <h1>Por que Escolher a Vidraçaria MBL vidros? </h1>
             </div>
-        
+
             <div className='tudao'>
                 <div className='descricoes'>
                     <h1>Qualidade garantida:</h1>
 
                     <p>Utilizamos apenas materiais de qualidade para <br /> garantir durabilidade e segurança.</p>
 
-                    
-                        <h1 className='experientes'>Profissionais Experientes:</h1>
-                    
+
+                    <h1 className='experientes'>Profissionais Experientes:</h1>
+
 
                     <p>Nossa Equipe é composta por vidraceiros <br /> qualificados e com vasta Experiencia no mercado.</p>
                 </div>
