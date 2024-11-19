3a46aef197db8ab821b863864ebbe6abce12e079
ryankkzy <ryan37370308@.com>
2024-11-19 07:50:25 -0300

    possivel ajuste final

--- src/pages/entrar/index.jsx
+++ src/pages/entrar/index.jsx
@@ -21,7 +21,7 @@ export default function Entrar () {
         }
 
         const url = 'http://4.172.207.208:5016/login'
-        //http://4.172.207.208:5016
+
         let resp = await axios.post(url, usuario)
 
         if (resp.data.erro){
